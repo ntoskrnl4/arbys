06fe98216b50e2b40a94f3f7d67126f07be0b506
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2018-08-30 01:48:46 +0000

    lru_cache is incompatible with lists passed as arguments

--- prefix.py
+++ prefix.py
@@ -2,7 +2,7 @@ from typing import List, Tuple, Union
 from functools import lru_cache
 
 
-@lru_cache(maxsize=256)
+# @lru_cache(maxsize=256)
 def check_prefix(message: str, prefixes: List[str]) -> Tuple[bool, Union[str, None]]:
 	check = message.lower().startswith
 	for prefix in prefixes:
