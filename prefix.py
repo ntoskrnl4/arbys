53e145a38a5b598392f772c28ce0155fc0fa6bc0
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2018-10-01 15:59:48 +0000

    fix command prefix bug ("pingggg" triggers "ping" command)

--- prefix.py
+++ prefix.py
@@ -5,7 +5,7 @@ from functools import lru_cache
 # @lru_cache(maxsize=256)
 def check_prefix(message: str, prefixes: List[str]) -> Tuple[bool, Union[str, None]]:
 	for prefix in prefixes:
-		if message.lower().startswith(prefix):
+		if message.lower().startswith(prefix) and (message.split(" ")[0] == prefix):
 			return True, prefix
 	return False, None
 
