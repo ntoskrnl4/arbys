00e75f3dc2b6f71c69c92cdf6a8308ca393311c5
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2018-10-06 01:13:37 +0000

    fix commands not triggering, hopefully fix join messages

--- modules/join_leave_msgs.py
+++ modules/join_leave_msgs.py
@@ -1,16 +1,14 @@
 from client import client
 import discord
 
-notification_channel = client.get_channel(473570993072504832)
-
 
 @client.member_join
 async def join_notification(member: discord.Member):
 	if member.guild.id == 364480908528451584:
-		await notification_channel.send(f"New member joined the server! Member {len(member.guild.members)}: {member.mention}")
+		await client.get_channel(473570993072504832).send(f"New member joined the server! Member {len(member.guild.members)}: {member.mention}")
 
 
 @client.member_leave
 async def leave_notification(member: discord.Member):
 	if member.guild.id == 364480908528451584:
-		await notification_channel.send(f"Member has left the server: {member.name}#{member.discriminator} ({member.display_name}) ({member.id})\nNew member count: {len(member.guild.members)}")
+		await client.get_channel(473570993072504832).send(f"Member has left the server: {member.name}#{member.discriminator} ({member.display_name}) ({member.id})\nNew member count: {len(member.guild.members)}")
