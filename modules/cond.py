6843cc0bf5d8654e04625b672f194830bd2b5c1f
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2018-10-15 19:40:40 +0000

    okay, this should actually do what I want

--- modules/cond.py
+++ modules/cond.py
@@ -1,4 +1,5 @@
 from client import client
+from io import BytesIO
 import requests
 import discord
 import time
@@ -17,20 +18,22 @@ client.long_help(cmd=cmd_name, mapping=detailed_help)
 last_cond_update = 0
 cond_cache_time = 7200
 
+last_file = None
 
 @client.command(trigger=cmd_name)
 async def command(command: str, message: discord.Message):
-	global cond_cache_time, last_cond_update
+	global cond_cache_time, last_cond_update, last_file
 	if command == "update":
 		last_cond_update = 1
 	if (time.time() - last_cond_update) >= cond_cache_time:
 		req = requests.get("http://www.hamqsl.com/solar101vhf.php")
-		with open("image.gif", "wb+") as tempfile:
-			for block in req.iter_content(16384):
-				tempfile.write(block)
-			last_cond_update = time.time()
-		await message.channel.send(file=discord.File("image.gif"))
+		data = b""
+		for block in req.iter_content(16384):
+			data += block
+		last_cond_update = time.time()
+		await message.channel.send(file=discord.File(BytesIO(data)))
+		last_file = data
 	else:
 		await message.channel.send(f"Using cached image from {int(time.time()-last_cond_update)} seconds ago ({int((time.time()-last_cond_update)/60)} minutes ago)")
-		await message.channel.send(file=discord.File("image.gif"))
+		await message.channel.send(file=discord.File(last_file))
 	return
