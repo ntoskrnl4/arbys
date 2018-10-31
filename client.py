4529e7c74f89b33f635a835d342c6cf5f54f9a4d
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2018-10-31 22:23:40 +0000

    Backport changes to upstream, from young-amateurs-rc/arbys
    Full list of changes:
     - Changed member_leave to member_remove (+ all references)
     - Added message and command counters
     - Shortened non-coroutine messages
     - Correctly exit the bot
     - Removed unused variables, plus the internal timer
     - Message handler decorator now takes an argument to receive bot's own
     - Checks on help to make sure that nothing ends up as blank
     - Bumped version

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 # 3.7: from __future__ import annotations
-from typing import List, Callable, Dict, Union
+from typing import List, Callable, Dict
 from prefix import check_prefix
-from exceptions import UserBotError, HandlerError, TimerException
+from exceptions import UserBotError, HandlerError
 
 import discord
 import time
@@ -10,6 +10,7 @@ import datetime
 import config
 import asyncio
 import traceback
+import sys
 
 
 def log_message(message: discord.Message):
@@ -28,13 +29,13 @@ def log_message(message: discord.Message):
 
 
 class FrameworkClient(discord.Client):
-	__version__ = "0.3.2-alpha"
+	__version__ = "0.4.0"
 
 	_ready_handlers: List[Callable[[], None]] = []
 	_shutdown_handlers: List[Callable[[], None]] = []
 	_message_handlers: List[Callable[[discord.Message], None]] = []
 	_member_join_handlers: List[Callable[[discord.Member], None]] = []
-	_member_leave_handlers: List[Callable[[discord.Member], None]] = []
+	_member_remove_handlers: List[Callable[[discord.Member], None]] = []
 
 	_command_lookup: Dict[str, Callable[[discord.Message, str], None]] = {}
 
@@ -56,8 +57,8 @@ class FrameworkClient(discord.Client):
 	prefixes: List[str] = []
 	default_prefix: str = None
 	log_all_messages: bool = config.log_messages
-
-	_trace_timer: float = None
+	message_count = 0
+	command_count = 0
 
 	def __init__(self, *args, **kwargs) -> None:
 		self.first_execution: float = None
@@ -83,24 +84,24 @@ class FrameworkClient(discord.Client):
 		if config.debug:
 
 			if any([not asyncio.iscoroutinefunction(x) for x in self._ready_handlers]):
-				log.critical("not all ready functions are coroutines, this could cause things to stop working or a fatal exception to be raised")
-				raise HandlerError("not all ready functions are coroutines, this could cause things to stop working or a fatal exception to be raised")
+				log.critical("not all ready functions are coroutines")
+				raise HandlerError("not all ready functions are coroutines")
 
 			if any([not asyncio.iscoroutinefunction(x) for x in self._shutdown_handlers]):
-				log.critical("not all shutdown functions are coroutines, this could cause things to stop working or a fatal exception to be raised")
-				raise HandlerError("not all shutdown functions are coroutines, this could cause things to stop working or a fatal exception to be raised")
+				log.critical("not all shutdown functions are coroutines")
+				raise HandlerError("not all shutdown functions are coroutines")
 
 			if any([not asyncio.iscoroutinefunction(x) for x in self._message_handlers]):
-				log.critical("not all message handlers are coroutines, this could cause things to stop working or a fatal exception to be raised")
-				raise HandlerError("not all message handlers are coroutines, this could cause things to stop working or a fatal exception to be raised")
+				log.critical("not all message handlers are coroutines")
+				raise HandlerError("not all message handlers are coroutines")
 
 			if any([not asyncio.iscoroutinefunction(x) for x in self._member_join_handlers]):
-				log.critical("not all member join handlers are coroutines, this could cause things to stop working or a fatal exception to be raised")
-				raise HandlerError("not all member join handlers are coroutines, this could cause things to stop working or a fatal exception to be raised")
+				log.critical("not all member join handlers are coroutines")
+				raise HandlerError("not all member join handlers are coroutines")
 
-			if any([not asyncio.iscoroutinefunction(x) for x in self._member_leave_handlers]):
-				log.critical("not all member leave handlers are coroutines, this could cause things to stop working or a fatal exception to be raised")
-				raise HandlerError("not all member leave handlers are coroutines, this could cause things to stop working or a fatal exception to be raised")
+			if any([not asyncio.iscoroutinefunction(x) for x in self._member_remove_handlers]):
+				log.critical("not all member leave handlers are coroutines")
+				raise HandlerError(f"not all member leave handlers are coroutines")
 
 			log.debug("all functions good to run (are coroutines)")
 
@@ -110,7 +111,6 @@ class FrameworkClient(discord.Client):
 	# ==========
 	# d.py event triggers
 	# ==========
-
 
 	async def on_ready(self):
 		for func in self._ready_handlers:
@@ -129,19 +129,23 @@ class FrameworkClient(discord.Client):
 				await func()
 			except Exception as e:
 				log.warning("Ignoring exception in shutdown coroutine (see stack trace below)", include_exception=True)
+			await asyncio.sleep(0.1)
+		client._do_cleanup()
+		sys.exit(0)
 
 	async def on_message(self, message: discord.Message):
+		self.message_count += 1
 		log_message(message)
 		if not self.active:
 			return
+		for func in self._message_handlers:
+			try:
+				await func(message)
+			except Exception:
+				log.warning("Ignoring exception in message coroutine (see stack trace below)", include_exception=True)
+
 		is_cmd, this_prefix = check_prefix(message.content, self.prefixes)
-		if not is_cmd:
-			for func in self._message_handlers:
-				try:
-					await func(message)
-				except Exception as e:
-					log.warning("Ignoring exception in message coroutine (see stack trace below)", include_exception=True)
-		else:
+		if is_cmd:
 			command = message.content[len(this_prefix):]
 			known_cmd, run_by = check_prefix(command, list(self._command_lookup.keys()))
 			if not known_cmd:
@@ -154,14 +158,14 @@ class FrameworkClient(discord.Client):
 		for func in self._member_join_handlers:
 			try:
 				await func(member)
-			except Exception as e:
+			except Exception:
 				log.warning("Ignoring exception in member_join coroutine (see stack trace below)", include_exception=True)
 
-	async def on_member_leave(self, member: discord.Member):
-		for func in self._member_leave_handlers:
+	async def on_member_remove(self, member: discord.Member):
+		for func in self._member_remove_handlers:
 			try:
 				await func(member)
-			except Exception as e:
+			except Exception:
 				log.warning("Ignoring exception in member_leave coroutine (see stack trace below)", include_exception=True)
 
 	# ==========
@@ -181,11 +185,14 @@ class FrameworkClient(discord.Client):
 
 			async def new_cmd(command: str, message: discord.Message) -> None:
 				try:
+					self.command_count += 1
+					# The following line is the gateway back into external code
 					await func(command, message)
 				except Exception:
+					timestamp = datetime.datetime.utcnow().__str__()
 					stackdump = traceback.format_exc()
 					embed = discord.Embed(title="Internal error", description=f"There was an error processing the command. Here's the stack trace, if necessary (this is also recorded in the log):\n```{stackdump}```", colour=0xf00000)
-					embed = embed.set_footer(text="Error occurred at " + str(datetime.datetime.utcnow()))
+					embed = embed.set_footer(text="Error occurred at " + timestamp)
 					await message.channel.send(embed=embed)
 					log.error(f"Error processing command: {message.content}", include_exception=True)
 
@@ -201,31 +208,52 @@ class FrameworkClient(discord.Client):
 	def member_join(self, func: Callable[[], None]):
 		self._member_join_handlers.append(func)
 		log.debug(f"registered new member_join handler {func.__name__}()")
+		return func
 
-	def member_leave(self, func: Callable[[], None]):
-		self._member_leave_handlers.append(func)
-		log.debug(f"registered new member_leave handler {func.__name__}()")
+	def member_remove(self, func: Callable[[], None]):
+		self._member_remove_handlers.append(func)
+		log.debug(f"registered new member_remove handler {func.__name__}()")
+		return func
 
-	def message(self, func: Callable[[], None]):
-		self._message_handlers.append(func)
-		log.debug(f"registered new message handler {func.__name__}()")
+	def message(self, receive_self: bool = True):
+		def inner_decorator(func: Callable[[discord.Message], None]):
+			async def wrapped_handler(message: discord.Message):
+				if not receive_self and message.author.id == client.user.id:
+					return
+				await func(message)
+			self._message_handlers.append(func)
+			log.debug(f"registered new message handler {func.__name__}()")
+			return wrapped_handler
+		return inner_decorator
 
 	def ready(self, func: Callable[[], None]):
 		self._ready_handlers.append(func)
 		log.debug(f"registered new ready handler {func.__name__}()")
+		return func
 
 	def shutdown(self, func: Callable[[], None]):
 		self._shutdown_handlers.append(func)
 		log.debug(f"registered new shutdown handler {func.__name__}()")
+		return func
 
 	def basic_help(self, title: str, desc: str, include_prefix: bool = True):
+		# check first that nothing's blank
+		if title.strip() == "" or desc.strip() == "":
+			log.critical("Blank content in help message: running the help command with this blank content *will* throw an error. Traceback logged under debug level.")
+			log.debug("".join(traceback.format_stack()))
+
 		if include_prefix:
 			self._basic_help.update({f"{self.default_prefix}{title}": desc})
 		else:
 			self._basic_help.update({f"{title}": desc})
 		log.debug(f"registered new basic_help entry under the title {title}")
 
-	def long_help(self, cmd: str, mapping: dict):
+	def long_help(self, cmd: str, mapping: Dict[str, str]):
+		if cmd.strip() == "" or "" in [x.strip() for x in [item for item in mapping.items()]]:
+			# if any blank values, throw an error
+			log.critical("Blank content in help message: running the help command with this blank content *will* throw an error. Traceback logged under debug level.")
+			log.debug("".join(traceback.format_stack()))
+
 		self._long_help[cmd] = mapping
 		log.debug(f"registered new long_help entry for command {cmd}")
 
