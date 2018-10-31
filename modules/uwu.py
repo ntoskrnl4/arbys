dd706036246a5d9fe7dc66b98bcda4247959dec5
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2018-10-31 20:51:45 +0000

    react even with spaces

--- modules/uwu.py
+++ modules/uwu.py
@@ -5,7 +5,7 @@ import key
 
 @client.message
 async def ft8_checker(message: discord.Message):
-	if (message.guild.id == 364480908528451584) and ("uwu" in message.content.lower()):
+	if (message.guild.id == 364480908528451584) and ("uwu" in message.content.lower().replace(" ", "")):
 		try:
 			await message.add_reaction(discord.utils.find(lambda x: x.id == key.uwu_emoji, message.guild.emojis))
 		except:  # thats ok, this is only a fun little joke
