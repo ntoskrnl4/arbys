107bfdc0a4cbe35b01fa32a9cc705acb2aacfbce
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2018-11-01 02:44:13 +0000

    change leave to remove to reflect d.py change and framework change

--- modules/join_leave_msgs.py
+++ modules/join_leave_msgs.py
@@ -15,7 +15,7 @@ async def join_notification(member: discord.Member):
 		await client.get_channel(473570993072504832).send(f"New member joined the server! Member {len(member.guild.members)}: {member.mention}")
 
 
-@client.member_leave
+@client.member_remove
 async def leave_notification(member: discord.Member):
 	if member.guild.id == 364480908528451584:
 		await client.get_channel(473570993072504832).send(f"Member has left the server: {member.name}#{member.discriminator} ({member.display_name}) ({member.id})\nNew member count: {len(member.guild.members)}")
