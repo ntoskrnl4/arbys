12d0feeecfd47039426fee795336e70b2013642c
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2018-11-11 15:48:05 +0000

    numbers are useless so let's log names instead of emoji IDs

--- modules/emoji_stats.py
+++ modules/emoji_stats.py
@@ -8,15 +8,15 @@ stats = Counter()
 @client.reaction_add
 async def reaction_use(reaction: discord.Reaction, source):
 	if reaction.custom_emoji:
-		stats[reaction.emoji.id] += 1
+		stats[reaction.emoji.name] += 1
 
 @client.reaction_remove
 async def unreact(reaction, source):
 	if reaction.custom_emoji:
-		stats[reaction.emoji.id] -= 1
+		stats[reaction.emoji.name] -= 1
 
 @client.message()
 async def check_messages(message):
-	for emoji_id in [int(re.search("\d{18}", x).group()) for x in re.findall(r"<:[a-zA-Z0-9_]{2,32}:\d{18}>", message.content)]:
+	for emoji_id in [re.search("<:[a-zA-Z0-9_]{2,32}:", x).group()[2:-1] for x in re.findall(r"<:[a-zA-Z0-9_]{2,32}:\d{18}>", message.content)]:
 		# List[int] cooresponding to custom emoji IDs
 		stats[emoji_id] += 1
