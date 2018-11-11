27a6cb351b8403ddff3c00e5b8d107795051c5a9
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2018-11-11 00:24:59 +0000

    indentation in command, add emoji statistics

---
+++ modules/emoji_stats.py
@@ -0,0 +1,22 @@
+from collections import Counter
+from client import client
+import discord
+import re
+
+stats = Counter()
+
+@client.reaction_add
+async def reaction_use(reaction: discord.Reaction, source):
+	if reaction.custom_emoji:
+		stats[reaction.emoji.id] += 1
+
+@client.reaction_remove
+async def unreact(reaction, source):
+	if reaction.custom_emoji:
+		stats[reaction.emoji.id] -= 1
+
+@client.message()
+async def check_messages(message):
+	for emoji_id in [int(re.search("\d{18}", x).group()) for x in re.findall(r"<:[a-zA-Z0-9_]{2,32}:\d{18}>", message.content)]:
+		# List[int] cooresponding to custom emoji IDs
+		stats[emoji_id] += 1
--- modules/fivenine.py
+++ modules/fivenine.py
@@ -4,12 +4,13 @@ import random
 
 client.basic_help(title="59", desc="Returns a picture showing 59 signal reception")
 
-fivenine = ["https://cdn.discordapp.com/attachments/377206780700393473/409899331584524308/20180204_213324.jpg",
-"https://cdn.discordapp.com/attachments/364489866119217153/412786093327384577/image.jpg",
-"https://cdn.discordapp.com/attachments/364489866119217153/412784280809242624/image.jpg",
-"https://cdn.discordapp.com/attachments/377206780700393473/414858156741754880/20180218_135538.jpg",
-"https://cdn.discordapp.com/attachments/377206780700393473/416371229428285467/20180222_180823.jpg",
-"https://cdn.discordapp.com/attachments/364489910759063552/440596315315896331/IMG_20180428_183441.jpg",
+fivenine = [
+	"https://cdn.discordapp.com/attachments/377206780700393473/409899331584524308/20180204_213324.jpg",
+	"https://cdn.discordapp.com/attachments/364489866119217153/412786093327384577/image.jpg",
+	"https://cdn.discordapp.com/attachments/364489866119217153/412784280809242624/image.jpg",
+	"https://cdn.discordapp.com/attachments/377206780700393473/414858156741754880/20180218_135538.jpg",
+	"https://cdn.discordapp.com/attachments/377206780700393473/416371229428285467/20180222_180823.jpg",
+	"https://cdn.discordapp.com/attachments/364489910759063552/440596315315896331/IMG_20180428_183441.jpg",
 ]
 
 @client.command(trigger="59", aliases=["fivenine"])
