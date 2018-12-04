01d45564091b7f62d597218185afe06ded7cc136
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2018-12-04 21:17:03 +0000

    :ntoskrnl_irl:

--- modules/uwu.py
+++ modules/uwu.py
@@ -4,9 +4,17 @@ import key
 
 
 @client.message()
-async def ft8_checker(message: discord.Message):
+async def hunter_irl(message: discord.Message):
 	if (message.guild.id == 364480908528451584) and ("uwu" in message.content.lower().replace(" ", "")):
 		try:
 			await message.add_reaction(discord.utils.find(lambda x: x.id == key.uwu_emoji, message.guild.emojis))
 		except:  # thats ok, this is only a fun little joke
+			pass
+
+@client.message()
+async def ntoskrnl_irl(message: discord.Message):
+	if (message.guild.id == 364480908528451584) and "<:ntoskrnl_irl:486325601750351882>" in message.content:
+		try:
+			await message.add_reaction(discord.utils.find(lambda x: x.id == 486325601750351882, message.guild.emojis))
+		except:
 			pass
