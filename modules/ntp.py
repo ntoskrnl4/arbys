5e8213a41345d37df9ceafd44ce881074f5d9da6
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2019-02-08 19:19:44 +0000

    Fix typo in string, remove pointless stat, and measure in us instead

--- modules/ntp.py
+++ modules/ntp.py
@@ -26,10 +26,9 @@ async def get_ntp_stats(command: str, message: discord.Message):
 		sync_status = [x for x in parts if x.startswith("sync_")][0]
 		stratum = int([x for x in parts if x.startswith("stratum=")][0][8:])
 		refid = [x for x in parts if x.startswith("refid=")][0][6:]  # this mess just gets the refid
-		offset = float([x for x in parts if x.startswith("offset=")][0][7:])
+		offset = float([x for x in parts if x.startswith("offset=")][0][7:])*1000
 		freq = float([x for x in parts if x.startswith("frequency=")][0][10:])
-		jitter = float([x for x in parts if x.startswith("sys_jitter=")][0][11:])
-		precision = 2**int([x for x in parts if x.startswith("precision=")][0][10:])
+		jitter = float([x for x in parts if x.startswith("sys_jitter=")][0][11:])*1000
 
 	embed = discord.Embed(title="NTP Statistics",
 						description="Current NTP statistics for the local machine")
@@ -37,13 +36,12 @@ async def get_ntp_stats(command: str, message: discord.Message):
 		embed.description = embed.description + "\n\nUnknown error while getting statistics."
 
 	if not alt:
-		embed = embed.add_field(name="Sync Status", value=f"`{sync_status}")
-		embed = embed.add_field(name="System Peer RefID", value=f"{refid}")
+		embed = embed.add_field(name="Sync Status", value=sync_status)
+		embed = embed.add_field(name="System Peer RefID", value=refid)
 		embed = embed.add_field(name="System Stratum", value=stratum)
-		embed = embed.add_field(name="Offset", value=f"{offset} ms")
-		embed = embed.add_field(name="Jitter", value=f"{jitter} ms")
+		embed = embed.add_field(name="Offset", value=f"{offset} us")
+		embed = embed.add_field(name="Jitter", value=f"{jitter} us")
 		embed = embed.add_field(name="Frequency Drift", value=f"{freq} ppm")
-		embed = embed.add_field(name="Timing Precision", value=f"{precision*1000:.2f} ms")
 
 	embed = embed.set_footer(text=datetime.datetime.utcnow().__str__())
 	await message.channel.send(embed=embed)
