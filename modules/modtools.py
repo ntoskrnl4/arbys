0ef3e8a3ed455fd0be5b4e29db2659468c7b913d
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2019-11-02 21:35:17 +0000

    Update modtools.py with new purge command and new channels

--- modules/modtools.py
+++ modules/modtools.py
@@ -1,36 +1,32 @@
 from client import client
+from modules import __common__
 from typing import List, Union
 
 import discord
 
-standard_channels: List[int] = [
+
+primary_channels: List[int] = [
 	364488710995050496,  # general
-	508720528035676167,  # general-voice-text
+]
+
+ham_radio_channels: List[int] = [
 	364489754839875586,  # general-ham
 	376859556858298378,  # weekly-net
+	364489808674029578,  # help-and-questions
 	364489783059152896,  # qsos-and-contesting
 	364489910759063552,  # equipment
-	364489808674029578,  # new-hams
 	427917747838779393,  # ham-voice-text
+]
+
+other_channels: List[int] = [
 	421832212120338442,  # college
 	427925486908473344,  # dev
 	403544637320593408,  # memes
 	377206780700393473,  # bot
-	526499196262678548,  # other-radio
 ]
 
-yarc_channels: List[int] = [
-	425443572720861205,  # general-yarc
-	513094354005393428,  # yarc-voice-text
-]
-
-standard_voice: List[int] = [
-	508127065053331456,  # general-voice
+public_voice_channels: List[int] = [
 	422047295941640212,  # ham-voice
-]
-
-yarc_voice: List[int] = [
-	425443651527770123,  # yarc-voice
 ]
 
 yarc_role = client.get_guild(364480908528451584).get_role(425443973700648971)
@@ -38,15 +34,16 @@ yarc_role = client.get_guild(364480908528451584).get_role(425443973700648971)
 
 @client.command(trigger="lock", aliases=["l"])
 async def lock(command: str, message: discord.Message):
-	if 377540982478209024 not in [x.id for x in message.author.roles]:
-		# user is not a mod
-		await message.channel.send(client.unknown_command)
-		return
-
 	if message.guild.id != 364480908528451584:
-		# wrong guild
-		await message.channel.send(client.unknown_command)
-		return
+		if message.guild.id != 452274699641159683:
+			# wrong guild
+			await message.channel.send("Invalid server")
+			return
+	else:
+		if 639519325287350272 not in [x.id for x in message.author.roles]:
+			# user is not a mod
+			await message.add_reaction("❌")
+			return
 
 	parts = command.split(" ")
 	# parts = ['lock', arg1]
@@ -55,78 +52,92 @@ async def lock(command: str, message: discord.Message):
 	target: Union[discord.TextChannel, discord.VoiceChannel] = None
 
 	if len(parts) == 1:
-		if message.author.voice.channel is not None:
+		if message.author.voice is not None:
 			target = message.author.voice.channel
 		else:
 			target = message.channel
 	else:
 		target = message.guild.get_channel(parts[1][2:-1])
-	if target.id not in standard_channels + yarc_channels + standard_voice + yarc_voice:
+	if (target.id not in primary_channels + ham_radio_channels + other_channels + public_voice_channels) and \
+		(message.guild.id != 452274699641159683):
 		await message.add_reaction("❌")
 		return
 
 	# Take away the proper write/speak permission
-	if target.id in standard_channels:
-		await target.set_permissions(target=target.guild.default_role, send_messages=False)
+	if isinstance(target, discord.TextChannel):
+		await target.set_permissions(target=target.guild.default_role, overwrite=discord.PermissionOverwrite(send_messages=False))
 		await message.delete()
 		return
-	if target.id in yarc_channels:
-		await target.set_permissions(target=yarc_role, send_messages=False)
-		await message.delete()
-		return
-	if target.id in standard_voice:
-		await target.set_permissions(target=target.guild.default_role, speak=False)
-		await message.delete()
-		return
-	if target.id in yarc_voice:
-		await target.set_permissions(target=yarc_role, speak=False)
+	if isinstance(target, discord.VoiceChannel):
+		await target.set_permissions(target=target.guild.default_role, overwrite=discord.PermissionOverwrite(speak=False))
 		await message.delete()
 		return
 
 
 @client.command(trigger="unlock", aliases=["ul"])
-async def lock(command: str, message: discord.Message):
-	if 377540982478209024 not in [x.id for x in message.author.roles]:
-		# user is not a mod
-		await message.channel.send(client.unknown_command)
-		return
-
+async def unlock(command: str, message: discord.Message):
 	if message.guild.id != 364480908528451584:
-		# wrong guild
-		await message.channel.send(client.unknown_command)
-		return
+		if message.guild.id != 452274699641159683:
+			# wrong guild
+			await message.channel.send("Invalid server")
+			return
+	else:
+		if 639519325287350272 not in [x.id for x in message.author.roles]:
+			# user is not a mod
+			await message.add_reaction("❌")
+			return
 
 	parts = command.split(" ")
-	# parts = ['lock', arg1]
+	# parts = ['unlock', arg1]
 
 	# Define it so we can use typing without cluttering it up later
 	target: Union[discord.TextChannel, discord.VoiceChannel] = None
 
 	if len(parts) == 1:
-		if message.author.voice.channel is not None:
+		if message.author.voice is not None:
 			target = message.author.voice.channel
 		else:
 			target = message.channel
 	else:
-		target = message.guild.get_channel(parts[1][2:-1])
-	if target.id not in standard_channels + yarc_channels + standard_voice + yarc_voice:
+		target = message.guild.get_channel(__common__.stripMentionsToID(parts[1]))
+	if (target.id not in primary_channels + ham_radio_channels + other_channels + public_voice_channels) and \
+		(message.guild.id != 452274699641159683):
 		await message.add_reaction("❌")
 		return
 
-	# Take away the proper write/speak permission
-	if target.id in standard_channels:
-		await target.set_permissions(target=target.guild.default_role, send_messages=True)
+	# Set the proper write/speak permission
+	if isinstance(target, discord.TextChannel):
+		await target.set_permissions(target=target.guild.default_role, overwrite=discord.PermissionOverwrite(send_messages=True))
 		await message.delete()
 		return
-	if target.id in yarc_channels:
-		await target.set_permissions(target=yarc_role, send_messages=True)
+	if isinstance(target, discord.VoiceChannel):
+		await target.set_permissions(target=target.guild.default_role, overwrite=discord.PermissionOverwrite(speak=True))
 		await message.delete()
 		return
-	if target.id in standard_voice:
-		await target.set_permissions(target=target.guild.default_role, speak=True)
-		await message.delete()
-		return
-	if target.id in yarc_voice:
-		await target.set_permissions(target=yarc_role, speak=True)
-		await message.delete()
-		return
+
+
+@client.command(trigger="purge", aliases=["nuke"])
+async def nuke_old_chat(command: str, message: discord.Message):
+	if message.guild.id != 364480908528451584:
+		if message.guild.id != 452274699641159683:
+			# wrong guild
+			await message.channel.send("Invalid server")
+			return
+	else:
+		if 639519325287350272 not in [x.id for x in message.author.roles]:
+			# user is not a mod
+			await message.add_reaction("❌")
+			return
+
+	parts = command.split(" ")
+	if len(parts) == 1:
+		count = 25
+	else:
+		count = int(parts[1])
+
+	target_msgs = await message.channel.history(limit=count).flatten()
+	assert isinstance(message.channel, discord.TextChannel)
+	assert isinstance(message.guild, discord.Guild)
+	await message.channel.delete_messages(target_msgs)
+	return
+
