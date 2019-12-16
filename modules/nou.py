188f6574976d5815ab16bc573a11791d564140f1
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2019-12-16 19:47:53 +0000

    Engage in a "no u" chain with the bot

--- modules/nou.py
+++ modules/nou.py
@@ -2,10 +2,18 @@ from client import client
 import discord
 
 
-@client.message()
+@client.message(receive_self=True)
 async def nou(message: discord.Message):
 	if message.content.lower().endswith("arbys shut down"):
 		try:
 			await message.channel.send("no u")
 		except:  # that's fine, we don't care
 			pass
+
+
+@client.message(receive_self=False)
+async def nou_chain(message: discord.Message):
+	if message.content.lower() == "no u":
+		hist = await message.channel.history(limit=9).flatten()
+		if hist[1].author.id == client.user.id:
+			await message.channel.send("no u")
